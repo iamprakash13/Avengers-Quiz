98dd278d90c514fa74cbb5886efb681dcaaf1771
Prakash King <32677400+iamprakash13@users.noreply.github.com>
2018-11-23 18:39:00 +0530

    Update quiz_page.dart

--- quiz/lib/pages/quiz_page.dart
+++ quiz/lib/pages/quiz_page.dart
@@ -18,37 +18,8 @@ class QuizPageState extends State<QuizPage> {
 
   Question currentQuestion;
   Quiz quiz = new Quiz([
-    new Question("Captain America's Shield is made of vibranium", true),
-    new Question("Captain America sponsered the black panther", true),
-    new Question("Martin Goodman started Marvel", true),
-     new Question("Nebula is an Adopted Daughter of InterGalactic WarLord", true),
-      new Question("Patsy Walker became Hellcat", true),
-       new Question("Thor's hammer is called Mjolnir", true),
-        new Question("Rick Jones was made an honorary Avenger", true),
- new Question("After thanos snap,the half of universe disintegrate", true),
-  new Question("Some of 'The Avengers' have appeared on 'The Tonight Show'", false),
-   new Question("Actress Emma Watson plays Black Widow", false),
-    new Question("Groot lost his life in Guardians of Galaxy", true),
-     new Question("Legendary hero of 20th Century who was honored by the 30th Century 'Guardians of Galaxy' was Spiderman", false),
-      new Question("Wanda and Pietro are twins", true),
-       new Question("Ava and Foster go into hiding", true),
-        new Question("Hank Pym is Real Avenger", true),
-         new Question("As the Hulk gets angrier, he gets stronger", true),
-          new Question("Iron Man & Thor discovered each others secret identity", true),
-         new Question("Hawkeye's real name is Steve Rodgers", false),
-          new Question("Pym recuses Hope form Quantam field", true),
-           new Question("Loki is called as 'god of mischief'", true),
-            new Question("Lang left drifting in the quantum realm", true),
-             new Question("The First Avengers movie come out in 2012", true),
-              new Question("Stark,Nebula,bannerM'Baku,Okoye,Rhodes,Rocket,Rogers,Romanoff and Thor survived after thanos snap", true),
-               new Question("Quicksilver's power is SuperSpeed", true),
-                new Question("Maria Hill and Nick Fury survived after thanos snap", false),
-                 new Question("The Sub-Mariner was the first hero to become a replacement Avenger,after the originals wanted to take a vacation", false),
-                  new Question("Thanos wielding the Infinity Gauntlet", true),
-                   new Question("Thanos watches a sunset on another planet", false),
-                   new Question("Stark senses that greater threat approaching Earth", true),
-                   new Question(" Fury is unable to transmit a signal ", false),
-                   new Question(" Captain Marvel is upcoming movie of avenger series", true),
+    new Question("sample question is here",true),
+    new Question("andriod belongs to steve jobs",false)
   ]);
   String questionText;
   int questionNumber;
