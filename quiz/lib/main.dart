1bc3060b7165fde72910632169916b5489bd4bfb
Prakash King <32677400+iamprakash13@users.noreply.github.com>
2018-11-23 16:25:27 +0530

    Create main.dart

---
+++ quiz/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './pages/landing_page.dart';
+
+// get into landing page
+
+void main() {
+  runApp(new MaterialApp(
+    home: new LandingPage(),
+  ));
+}
